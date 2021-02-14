ef9f879dc0cd3b9721abde23daf2e3a55b8ba6cf
paraglande03 <paraglande03@gmail.com>
2021-02-14 19:07:42 +0530

    Added length comparison

--- LineCalculation.java
+++ LineCalculation.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class LineCalculation {
 
     double x1,x2,y1,y2;
+    double length;
     public void values(){
         System.out.println("Enter values of points");
 
@@ -14,10 +15,32 @@ public class LineCalculation {
         System.out.println("Points are: ("+x1+","+x2+")"+"("+y1+","+y2+")");
     }
 
-    public void calculation(){
-        double length;
+    public double calculation(){
+
         length=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y2));
-        System.out.println("length=" + length);
+        return length;
+
+    }
+    public void EqualCheck(){
+        double length1, length2;
+        System.out.println("For line 1:---");
+        values();
+        calculation();
+        length1=length;
+        System.out.println("length of line 1="+length1);
+        System.out.println("For line 2:---");
+        values();
+        calculation();
+        length2=length;
+        System.out.println("length of line 2="+length2);
+
+            if(length1==length2){
+                System.out.println("Lines are equal");
+            }
+            else{
+                System.out.println("Lines are not equal");
+            }
+
 
     }
 }
--- LineProblem.java
+++ LineProblem.java
@@ -1,7 +1,9 @@
 public class LineProblem {
     public static void main(String[] args) {
     LineCalculation lineCalculation=new LineCalculation();
-    lineCalculation.values();
-    lineCalculation.calculation();
+
+    lineCalculation.EqualCheck();
+
+
     }
 }
